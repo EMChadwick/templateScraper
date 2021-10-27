fc268fa392144f111ff92d514f6db6385a2072f5
Ed Chadwick <47214282+EMChadwick@users.noreply.github.com>
2021-10-27 14:25:35 +0100

    Flexible starting point
    You can now supply any page one the site as a starting point, and it will navigate from there.
    
    Potentially dropped a small set of links due to filtering changes, but I'm getting there.

--- templateScraper.py
+++ templateScraper.py
@@ -4,14 +4,18 @@ Scraper Template
 """
 import requests, csv, re
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 pageList = []
-excludeList=[] #populate this with keywords found in URLs you don't want to check 
-start = input('Enter Starting url: ') #the starting page to branch from - usually the index page is a good start
+excludeList=['@'] #populate this with keywords found in URLs you don't want to check 
+start = input('Enter Starting url: ') #the starting page to branch from - currently only supports index pages
 if 'http' not in start:
     start = 'https://' + start
+domain = urlparse(start).netloc
+print('domain is '+domain)
 pageList.append(start)
 for page in pageList:
     linkList = []
+    print("Searching "+page)
     response = requests.get(page)
     if(response.status_code == 200):
         #get all links on the page
@@ -29,25 +33,32 @@ for page in pageList:
             """
             Add checks here to look for links of a certain kind to store if that's what you're looking for.
             """
-            #this part appends the starting URL to the current one if it's a local link. Works OK, but still improve it.
-            if (t[0] == '/'):
-                t = start + t
-            if (re.match('[a-z]',t[0])) and ('http' not in t):
-                t = start + "/" + t
-            #add appropriate links to the pagelist to be checked if they aren't there already
-            if (start in t) and (t not in pageList) and not(any(bad in t for bad in (excludeList))):
-                pageList.append(t)
+            #this part curates the links. Works OK, but still improve it - remove parameters and things that aren't pages
+            print("checking: "+t)
+            if(len(t)>0):
+                
+                if (t[0] == '/'): 
+                    t = page + t
+                if (re.match('[a-z]',t[0])) and ('http' not in t):
+                    if(page.split('/')[-1] == domain)or re.search('^index',t):
+                        t = 'https://'+domain + "/" + t
+                    else:
+                        print('yeet')
+                        t = page[0: page.rindex('/')]+'/' + t
+                #add appropriate links to the pagelist to be checked if they aren't there already
+                if (domain in t) and (t not in pageList) and not(any(bad in t for bad in (excludeList))):
+                    pageList.append(t)
+                    print('adding '+t)
     else:
-        print("Page "+page+"returned error "+str(response.status_code))
+        print("Page "+page+" returned error "+str(response.status_code))
         
         
 #chuck everything into a csv file
-"""
 with open('siteURLs.csv', mode = 'w') as csvFile:
     csvWriter = csv.writer(csvFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     csvWriter.writerow(['Page URL'])
     for entry in pageList:
         csvWriter.writerow([entry])
-"""
+
 print(len(pageList))
 print(pageList)
